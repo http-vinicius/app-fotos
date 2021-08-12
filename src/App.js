b82277deaa9d2577927a73bc91d5565e063ba50f
vinicius <vinicius_jose16@hotmail.com>
2021-08-12 09:46:03 -0300

    Header Footer e Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div>App React</div>
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
   );
 }
 
